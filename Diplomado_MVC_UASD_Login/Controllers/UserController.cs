68c6518239c20c22171df647e8fd1189eb60e637
Jonathan Lorenzo Contrera <jonathanlorenzoc_rt@outlook.es>
2020-01-11 13:35:30 -0400

    Actualizacion

--- Diplomado_MVC_UASD_Login/Controllers/UserController.cs
+++ Diplomado_MVC_UASD_Login/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -45,9 +46,42 @@ namespace Diplomado_MVC_UASD_Login.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                User oldUser = DataDataContext.Users.FirstOrDefault(u => u.IdUser == user.IdUser);
+                if (user != null)
+                {
+                    DataDataContext.Users.InsertOnSubmit(user);
+                    DataDataContext.SubmitChanges();
+                }
             }
             return View("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            User user = DataDataContext.Users.FirstOrDefault(u => u.IdUser == id);
+            return View(user);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            User user = DataDataContext.Users.FirstOrDefault(u => u.IdUser == id);
+            return View(User);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User oldUser = DataDataContext.Users.FirstOrDefault(u => u.IdUser == user.IdUser);
+                if (oldUser != null)
+                {
+                    DataDataContext.Users.DeleteOnSubmit(oldUser);
+                    DataDataContext.SubmitChanges();
+                    return View("Index");
+                }
+            }
+            return View(user);
         }
     }
 }
